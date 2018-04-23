c76203d19e8510761c7cd694348c3b73635cf832
yang-yeonsu <yang-yeonsu@10.37.129.2>
2018-04-23 20:59:16 +0900

    update index

--- market/src/main/java/branch/controller/BranchController.java
+++ market/src/main/java/branch/controller/BranchController.java
@@ -1,20 +1,8 @@
 package branch.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import branch.vo.Branch;
 
 @Controller
-@RequestMapping("/")
 public class BranchController {
 
-	@GetMapping(path = "/branches")
-	public Branch listBranch() {
-		System.out.println("call listBranch");
-		return null;
-		
-	}
-	
 }
--- market/src/main/java/home/HomeController.java
+++ market/src/main/java/home/HomeController.java
@@ -1,5 +1,19 @@
 package home;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import branch.vo.Branch;
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
 
+	@GetMapping(path = "/index")
+	public Branch listBranch() {
+		System.out.println("call listBranch");
+		return null;
+	}
+	
 }
